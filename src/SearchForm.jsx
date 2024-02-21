f1d91fee78806317bcad173000757e756692d060
Mihai Childesco <mihai.childesco@gmail.com>
2024-02-21 10:56:08 +0200

    updated placeholder from cat to dog

--- src/SearchForm.jsx
+++ src/SearchForm.jsx
@@ -17,7 +17,7 @@ const SearchForm = () => {
           type="text"
           className="form-input search-input"
           name="search"
-          placeholder="cat"
+          placeholder="dog"
         />
         <button className="btn">search</button>
       </form>
